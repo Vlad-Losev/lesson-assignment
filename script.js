3b6784fa5c723f567b0f5d6c75a51599223054ee
Vlad Losev <Vladius1744@yandex.ru>
2022-10-31 20:02:35 +0500

    Правки

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
 'use strict';
 
-// let NameProduct = 'bananas';
-// let QuantityGoods = 5;
-// let ProductCategory = 'FoodProduct';
-// let PriceProduct = 20;
+const nameproduct = prompt('Какое наименование товара?');
+console.log(typeof nameproduct);
 
-// console.log('NameProduct:','bananas');
+const quantitygoods = Number(prompt('Какое количествро товара?'));
+console.log(typeof quantitygoods);
+  // , parseInt(quantitygoods, Number));
 
-// console.log(PriceProduct * QuantityGoods);
+const productcategory = prompt('Какая категория товара?');
+console.log(typeof productcategory);
 
-// console.log(20 * 5);
+const priceproduct = Number(prompt('Какая цена товара?'));
+console.log(typeof priceproduct);
 
-// let TotalAmount;
+const totalamount = priceproduct * quantitygoods;
 
-// console.log(TotalAmount = PriceProduct * QuantityGoods);
-
-const NameProduct = prompt('Какое наименование товара?');
-console.log(typeof NameProduct);
-
-const QuantityGoods = prompt('Какое количествро товара?');
-console.log(typeof QuantityGoods, parseInt(QuantityGoods, Number));
-
-const ProductCategory = prompt('Какая категория товара?');
-console.log(typeof ProductCategory);
-
-const PriceProduct = prompt('Какая цена товара?');
-console.log(typeof PriceProduct);
-
-const TotalAmount = PriceProduct * QuantityGoods;
-
-console.log('В корзине', QuantityGoods, NameProduct,'на сумму', TotalAmount, 'золотых');
+console.log('В корзине', quantitygoods, nameproduct,'на сумму', totalamount, 'золотых');
